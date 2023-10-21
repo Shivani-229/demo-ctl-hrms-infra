21ad596b2aedb2a267cdb76c5f8c68fe339c99c5
codvatechlabs <78929919+codvatechlabs@users.noreply.github.com>
2023-10-21 17:05:57 +0530

    Update user_data.sh

--- user_data.sh
+++ user_data.sh
@@ -3,4 +3,4 @@ yum update -y
 yum install -y httpd
 systemctl start httpd.service
 systemctl enable httpd.service
-echo "ASG Demo !!!, I am $(hostname -f) hosted by Terraform" > /var/www/html/index.html
+echo "ASG Demo 2.0 !!!, I am $(hostname -f) hosted by Terraform" > /var/www/html/index.html
