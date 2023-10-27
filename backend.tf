877a35eef9deb2a96dee0cad6d8d393e072b870b
Shivani-229 <147433204+Shivani-229@users.noreply.github.com>
2023-10-27 20:57:55 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket         = "ctl-tf-state-lock-demo"
-    key            = "hrms-demo"
-    region         = "ap-south-1"
-    dynamodb_table = "tf-lock-state"
+    bucket         = "shivani-bucket-21"
+    key            = "tfdemo/backend"
+    region         = "ap-south-2"
+    dynamodb_table = "ctl-db-18"
   }
 }
